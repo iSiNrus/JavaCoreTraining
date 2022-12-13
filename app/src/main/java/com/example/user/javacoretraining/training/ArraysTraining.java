9dacd48a3bdcc7cc4c2b5e80365cf070c22bb637
iSiNrus <magnit_322@rambler.ru>
2022-12-13 16:32:37 +0400

    ArraysTraining

--- app/src/main/java/com/example/user/javacoretraining/training/ArraysTraining.java
+++ app/src/main/java/com/example/user/javacoretraining/training/ArraysTraining.java
@@ -19,7 +19,18 @@ public class ArraysTraining {
      * @return отсортированный массив
      */
     public int[] sort(int[] valuesArray) {
-        //TODO: implement it
+        for(int j = 1; j < valuesArray.length; j++){
+            boolean f = false;
+            for(int i=0; i<valuesArray.length-j; i++){
+                if(valuesArray[i]>valuesArray[i+1]){
+                    int t = valuesArray[i];
+                    valuesArray[i] = valuesArray[i+1];
+                    valuesArray[i+1] = t;
+                    f = true;
+                }
+            }
+            if(!f) break;
+        }
         return valuesArray;
     }
 
@@ -32,8 +43,13 @@ public class ArraysTraining {
      * @return максимальное число или 0
      */
     public int maxValue(int... values) {
-        //TODO: implement it
-        return 0;
+        if(values.length==0) return 0;
+        else {
+            int max = values[0];
+            for(int v : values)
+                if(v > max) max = v;
+            return max;
+        }
     }
 
     /**
@@ -44,8 +60,12 @@ public class ArraysTraining {
      * @return входящий массив в обратном порядке
      */
     public int[] reverse(int[] array) {
-        //TODO: implement it
-        return new int[]{};
+        for(int i=0; i<array.length/2; i++){
+            int t = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = t;
+        }
+        return array;
     }
 
     /**
@@ -59,8 +79,18 @@ public class ArraysTraining {
      * @return массив из чисел Фибоначчи
      */
     public int[] fibonacciNumbers(int numbersCount) {
-        //TODO: implement it
-        return new int[]{};
+        if(numbersCount<1) return new int[]{};
+        if(numbersCount==1) return new int[]{1};
+        if(numbersCount==2) return new int[]{1,1};
+        else {
+            int[] array = new int[numbersCount];
+            array[0] = 1;
+            array[1] = 1;
+            for(int i=2; i<array.length; i++){
+                array[i] = array[i-1] + array[i-2];
+            }
+            return array;
+        }
     }
 
     /**
@@ -72,7 +102,12 @@ public class ArraysTraining {
      * элементов
      */
     public int maxCountSymbol(int[] array) {
-        //TODO: implement it
-        return 0;
+        int maxCount = 0;
+        for (int j : array) {
+            int count = 0;
+            for (int k : array) if (j == k) count++;
+            if (maxCount < count) maxCount = count;
+        }
+        return maxCount;
     }
 }
